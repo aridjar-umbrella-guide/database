45c5af2daf74d89c4506b397d484ee7e09678eca
Aridjar <cedric@paumard.net>
2019-03-20 15:38:25 +0100

    add bootstrap 4

--- config/dev.exs
+++ config/dev.exs
@@ -6,3 +6,11 @@ config :database, Database.Repo,
   database: System.get_env("POSTGRES_DB_DEV"),
   hostname: System.get_env("POSTGRES_HOST"),
   pool_size: 10
+
+# This part will only be used if we don't want to use Docker, and if you don't have env set up.
+# config :database, Database.Repo,
+#   username: "user",
+#   password: "password",
+#   database: "test_dev",
+#   hostname: "127.0.0.1",
+#   pool_size: 10
